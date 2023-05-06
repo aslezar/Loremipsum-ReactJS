91c44e14c5feb817d86511f67d975c1cc85a42e7
Instance01 <97354675+aslezar@users.noreply.github.com>
2023-05-07 01:11:47 +0530

    07May2023

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let amount = parseInt(count);
+		amount = amount <= 0 ? 1 : amount > 8 ? 8 : amount;
+		setText(data.slice(0, amount));
+	};
+	return (
+		<section className="section-center">
+			<h3>Bored of Boring Lorem ipsum?...</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">Paragraphs: </label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">
+					Generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
+	return <h2>lorem ipsum project setup</h2>;
 }
 
 export default App;
